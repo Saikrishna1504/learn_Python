788678ac3fc34e37b36397d75a71e9c409381c2e
Saikrishna1504 <saikrishna26918@gmail.com>
2023-10-17 00:04:53 +0530

    conversions,example and operators
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

---
+++ tutorial/conversions_operators.py
@@ -0,0 +1,27 @@
+# Finding legnth of given setence
+print(len("Hi,Welcome to metaverse")) // len function won't work for int data type.
+
+# Storing len function
+length=len("Hi,Welcome to metaverse")
+print("it has " +"str(length)"+ " characters")
+print(type(length)) // for checking which data type it is.
+
+# Exercise 
+// adding  digits of given number
+
+num = input("enter two digit number")
+first_no=num[0]
+second_no=num[1]
+print(int(first_no) + int(second_no))
+
+# Operators
+a = 5
+a += 2 | a %=2 | a //=2 | a -=2 | a *=2
+print(a)  // we can assign and add any in same way.
+print(a<5) --> False
+print(a>5) --> False
+print(a=5) --> True 
+print(a!=5) --> False
+
+
+
